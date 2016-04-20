caa66c8f0bb8b11f651aff3c04b32a38949c873a
Jaydyn Zimmerman <CabbaLens@users.noreply.github.com>
2016-04-19 22:08:02 -0400

    Update slui.bat

--- slui.bat
+++ slui.bat
@@ -1,21 +1,19 @@
 @echo off
-rem this isn't the exact code of either of the programs, but it uses the same commands.
+rem this isn't the exact code of either of the programs
 title slui.exe terminator
-rem msg * Slui terminator is running in the background.
-rem timeout 300
+rem echo waiting...
+rem timeout 250
 :loop
 tasklist /fi "imagename eq slui.exe" | find /i "slui.exe" > nul
 if not errorlevel 1 (
 echo %time%: slui found 
 taskkill /f /im "slui.exe"
-echo timeout 60 >> tempfile.bat
 echo Pausing for 420 seconds.
-timeout 420 >nul
-del tempfile.bat
+timeout 420
 goto loop
 ) else (
 echo %time%: slui NOT found :)
 echo %time%: Pausing for 10 seconds.
-timeout 10 >nul
+timeout 10
 goto loop
 )
